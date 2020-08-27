6993f793ec4f2c96d90629b826ec0f72d0478f7c
Georgi Morozov <georgimorozov@gmail.com>
2020-08-27 13:42:22 -0400

    no message

--- resources/views/components/field-details-postman.blade.php
+++ resources/views/components/field-details-postman.blade.php
@@ -1,6 +1,5 @@
 
-    <code><b>{{ $name }}</b></code>&nbsp; @if($type)<small>{{ $type }}</small>@endif @if(!$required)
-        <i>optional</i>@endif
-    <br>
-    {!! $description !!}
-
+<code><b>{{ $name }}</b></code>&nbsp; @if($type)<small>{{ $type }}</small>@endif @if(!$required)
+    <i>optional</i>@endif
+<br>
+{!! $description !!}
--- resources/views/partials/description.blade.php
+++ resources/views/partials/description.blade.php
@@ -1,8 +1,7 @@
-
 @if(count($route['urlParameters']))
     <h4 class="fancy-heading-panel"><b>URL Parameters</b></h4>
     @foreach($route['urlParameters'] as $attribute => $parameter)
-        @component('scribe::components.field-details-postman', [
+        @component('scribe::components.field-details', [
           'name' => $attribute,
           'type' => null,
           'required' => $parameter['required'] ?? true,
@@ -14,7 +13,7 @@
 @if(count($route['queryParameters']))
     <h4 class="fancy-heading-panel"><b>Query Parameters</b></h4>
     @foreach($route['queryParameters'] as $attribute => $parameter)
-        @component('scribe::components.field-details-postman', [
+        @component('scribe::components.field-details', [
           'name' => $attribute,
           'type' => null,
           'required' => $parameter['required'] ?? true,
@@ -24,9 +23,9 @@
     @endforeach
 @endif
 @if(count($route['bodyParameters']))
-    <h4 class="fancy-heading-panel"><b>Body Parameters</b></h4><br>
+    <h4 class="fancy-heading-panel"><b>Body Parameters</b></h4>
     @foreach($route['bodyParameters'] as $attribute => $parameter)
-        @component('scribe::components.field-details-postman', [
+        @component('scribe::components.field-details', [
           'name' => $attribute,
           'type' => $parameter['type'] ?? null,
           'required' => $parameter['required'] ?? true,
